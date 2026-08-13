cbcfd7e4a40075d3608f883fd56567fc4f4d2282
portello21 <allanvitorportello21@gmail.com>
2026-08-13 04:38:35 -0400

    chore: remove legacy custom composer bootstrap

--- frontend/chat_input/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import ChatInput from "./ChatInput";
-
-ReactDOM
-  .createRoot(document.getElementById("root")!)
-  .render(<ChatInput />);
